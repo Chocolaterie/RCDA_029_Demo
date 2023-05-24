f7cc830668a34285b11acea973a6e3a9694c55bc
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-24 16:58:53 +0200

    Demo

--- src/main/java/fr/example/demo/controller/HomeController.java
+++ src/main/java/fr/example/demo/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.example.demo.injection.ServicePerson;
 
@@ -18,6 +21,36 @@ public class HomeController {
 		
 		servicePerson.test();
 		
+		return "home";
+	}
+	
+	@GetMapping("default-article")
+	public String defaultArticle(Model model) {
+		// Creer une donnée
+		String articleName = "Pizza Ananas c'est la vie";
+		
+		// Envoyer la donnée dans le model
+		model.addAttribute("articleName", articleName);
+		
+		// Retourner la page
+		return "article.html";
+	}
+	
+	@GetMapping("article/{id}")
+	public String article(@PathVariable("id") int id ) {
+		
+		System.out.println(String.format("Voici l'id transféré depuis l'url %d", id));
+		
+		// Affiche la page home
+		return "home";
+	}
+	
+	@GetMapping("catalogue")
+	public String catalogue(@RequestParam("type") String type) {
+		
+		System.out.println(String.format("Voici le type %s", type));
+		
+		// Affiche la page home
 		return "home";
 	}
 	

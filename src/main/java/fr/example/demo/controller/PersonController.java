a180506078a802352fdce27a5934c7e3880caa70
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-25 11:35:18 +0200

    avancée

--- src/main/java/fr/example/demo/controller/PersonController.java
+++ src/main/java/fr/example/demo/controller/PersonController.java
@@ -3,7 +3,9 @@ package fr.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.example.demo.bo.Person;
 
@@ -16,6 +18,17 @@ public class PersonController {
 		// Créer une personne mocké pour tester
 		Person person = new Person(slug, "Sylvain", "DURIF");
 		
+		// Envoyer dans le model
+		model.addAttribute("person", person);
+		
 		return "person/person";
+	}
+	
+	@PostMapping("person")
+	public String personSubmit(@ModelAttribute Person person) {
+		
+		System.out.println(String.format("Person : %s", person.getFirstname()));
+		
+		return "redirect:/";
 	}
 }

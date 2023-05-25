31bc2c364196d61a6bc024753d80013b7c8d8bb5
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-25 13:50:21 +0200

    Demo thymleaf

---
+++ src/main/java/fr/example/demo/bll/PersonService.java
@@ -0,0 +1,35 @@
+package fr.example.demo.bll;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import fr.example.demo.bo.Person;
+
+@Service
+public class PersonService {
+
+	List<Person> persons;
+	
+	PersonService() {
+		persons = Arrays.asList(new Person[]{
+				new Person("isaac", "Isaac", "Chocolatine"),
+				new Person("lucas", "Lucas", "PizzaAnanas"),
+				new Person("ludovic", "Ludovic", "CrevetteNutella"),
+		});
+	}
+	
+	public List<Person> getAll() {
+		return persons;
+	}
+	
+	public Person getPersonBySlug(String slug) {
+		for (Person person : persons) {
+			if (person.getSlug().equals(slug)) {
+				return person;
+			}
+		}
+		return null;
+	}
+}
--- src/main/java/fr/example/demo/controller/PersonController.java
+++ src/main/java/fr/example/demo/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,33 +12,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import fr.example.demo.bll.PersonService;
 import fr.example.demo.bo.Person;
 
 @Controller
 public class PersonController {
 
+	@Autowired
+	PersonService personService;
 	
 	@GetMapping("show-persons")
-	public String showAllPerson(Model model) {
-		// 1 : Les données en dur
-		List<Person> persons = Arrays.asList(new Person[]{
-				new Person("isaac", "Isaac", "Chocolatine"),
-				new Person("lucas", "Lucas", "PizzaAnanas"),
-				new Person("ludovic", "Ludovic", "CrevetteNutella"),
-		});
+	public String showAllPerson(Model model) {		
+		// 1 : Envoyer les données dans le model pour la vue
+		model.addAttribute("persons", personService.getAll());
 		
-		// 2 : Envoyer les données dans le model pour la vue
-		model.addAttribute("persons", persons);
-		
-		// 3 : Retourner la vue
+		// 2 : Retourner la vue
 		return "person/persons-list";
 	}
 	
 	@GetMapping("person/{slug}")
 	public String person(@PathVariable("slug") String slug, Model model) {
 		
-		// Créer une personne mocké pour tester
-		Person person = new Person(slug, "Sylvain", "DURIF");
+		// Récupérer une personne mock
+		Person person = personService.getPersonBySlug(slug);
 		
 		// Envoyer dans le model
 		model.addAttribute("person", person);

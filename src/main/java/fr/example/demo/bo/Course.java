d0aa2d77e782cec05a649c4642e3be76b5763cba
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-31 16:59:44 +0200

    Demo

---
+++ src/main/java/fr/example/demo/bo/Course.java
@@ -0,0 +1,19 @@
+package fr.example.demo.bo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+}
